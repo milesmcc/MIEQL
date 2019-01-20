4e72e6fcd2a3b6b15578e4920a0d93b8b4650162
R. Miles McCain <0@rmrm.io>
2019-01-20 00:00:12 -0500

    Add CLI

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,22 @@ extern crate ieql;
 extern crate clap;
 #[macro_use]
 extern crate log;
-extern crate simplelog;
+extern crate futures;
 extern crate hyper;
 extern crate ron;
+extern crate simplelog;
+extern crate reqwest;
+
+use std::net::SocketAddr;
 
 use clap::{App, Arg, SubCommand};
 
 use ieql::query::query::{Query, QueryGroup};
 use std::path::Path;
 
-mod master;
 mod client;
+mod master;
+mod util;
 
 fn main() {
     simplelog::CombinedLogger::init(vec![simplelog::TermLogger::new(
@@ -30,11 +35,12 @@ fn main() {
         .subcommand(
             SubCommand::with_name("client")
                 .about("Act as the client")
-                .args_from_usage("-m, --master=<master url> 'The url of the master'")
+                .args_from_usage("-m, --master=[master url] 'The url of the master (http://0.0.0.0:3000 by default)'"),
         )
         .subcommand(
             SubCommand::with_name("master")
                 .about("Act as the master")
+                .args_from_usage("-b, --bind=[bind address] 'The network address to bind to (0.0.0.0:3000 by default)'"),
         )
         .get_matches();
     run(matches);
@@ -43,12 +49,17 @@ fn main() {
 fn run(matches: clap::ArgMatches) {
     match matches.subcommand() {
         ("client", Some(m)) => {
-            let master_url = m.value_of("master").expect("master URL is required");
-            client::main(master_url);
-        },
+            let master_url = m.value_of("master").unwrap_or("http://0.0.0.0:3000");
+            client::main(String::from(master_url));
+        }
         ("master", Some(m)) => {
-            master::main(([0,0,0,0], 3000).into());
-        },
+            let bind_address: SocketAddr = m
+                .value_of("bind")
+                .unwrap_or("0.0.0.0:3000")
+                .parse()
+                .expect("invalid bind address provided");
+            master::main(bind_address);
+        }
         _ => error!("no valid command specified; try running with `--help`."),
     }
 }
