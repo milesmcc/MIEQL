a4808eecfb0de6f3813e3243e5593670925f1f11
R. Miles McCain <0@rmrm.io>
2019-01-20 17:07:55 -0500

    Add half-baked client streaming functionality

--- src/client.rs
+++ src/client.rs
@@ -1,4 +1,10 @@
+use flate2::read::GzDecoder;
+use futures::{Future, IntoFuture};
 use ieql::query::query::QueryGroup;
+use nom::{MemProducer, Producer};
+use rusoto_s3;
+use rusoto_s3::S3;
+use std::io::Read;
 
 pub fn main(master_url: String) {
     // Test connection
@@ -16,6 +22,10 @@ pub fn main(master_url: String) {
         }
     }
 
+    // Create dataset client
+    let s3_client = rusoto_s3::S3Client::new(rusoto_core::region::Region::UsEast1);
+    info!("Launched S3 client...");
+
     // Get queries
     let queries_url = format!("{}/queries", &master_url);
     let queries_response = reqwest::get(queries_url.as_str())
@@ -32,17 +42,43 @@ pub fn main(master_url: String) {
     // Stream and process an archive
     loop {
         let data_url = format!("{}/data", &master_url);
-        let mut data_response: reqwest::Response = reqwest::get(data_url.as_str())
-            .expect("unable to retrieve data url!");
+        let mut data_response: reqwest::Response =
+            reqwest::get(data_url.as_str()).expect("unable to retrieve data url!");
         if data_response.status().as_u16() == 204 {
             info!("no more data left!");
             info!("shutting down...");
             break;
         }
         let url_to_stream = data_response.text().unwrap();
-        info!(
-            "found data `{}` to process",
-            url_to_stream
-        );
+        info!("found data `{}` to process", url_to_stream);
+        let request = rusoto_s3::GetObjectRequest {
+            bucket: String::from("commoncrawl"),
+            key: url_to_stream,
+            ..Default::default()
+        };
+        let result = match s3_client.get_object(request).sync() {
+            Ok(value) => value,
+            Err(err) => {
+                error!(
+                    "encountered issue while loading object (`{}`), skipping...",
+                    err
+                );
+                continue;
+            }
+        };
+        let mut stream = result.body.unwrap().into_blocking_read();
+        let mut decoder = GzDecoder::new(stream);
+        let mut consumer = warc_parser::WarcConsumer::new();
+        let mut buffer = [0u8; 65536];
+        let resp = decoder.read(&mut buffer);
+        info!("{:?}", String::from_utf8_lossy(&buffer));
+        break;
+        // for _ in 0..1024 {
+        //     decoder.read(&mut buffer);
+        //     let mut producer = MemProducer::new(&buffer, 4096);
+        //     producer.apply(&mut consumer);
+        // }
+        info!("{} records found in buffer", consumer.counter);
+        info!("{:?}", consumer.last_record);
     }
 }
